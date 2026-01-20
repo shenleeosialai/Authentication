abf23e45ede6a5c4d45d8f1dec1f528695996f56
shen <shenleekhalid@gmail.com>
2026-01-20 22:46:07 +0300

    simple dashboard view to redirect users after log in and settings for django to automatically use this urls

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from .import views
 app_name = 'account'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('dahboard/',views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,16 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
 
 def user_login(request):
     if request.method == 'POST':
