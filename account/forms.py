d5a67b363a6b763f6c025db736482543aa167fe2
shen <shenleekhalid@gmail.com>
2026-01-21 23:55:13 +0300

    user registration form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username eg. john doe'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password eg 123@wrf'}))
+
+
+class UserRegistrationForm(forms.MOdelForm):
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput
+    )
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'first_name', 'email')
