d8ce6b8e2220367a09e1410745f7ce470dc8cb75
shen <shenleekhalid@gmail.com>
2026-01-22 18:16:15 +0300

    add register to urls

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,6 @@ app_name = 'account'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register')
 ]
