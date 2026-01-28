bd412478f20a88213a3c4c5c13792616a157b1ce
shen <shenleekhalid@gmail.com>
2026-01-28 23:46:58 +0300

    adding googleOAuth2 to authentication backends

--- authentication/settings.py
+++ authentication/settings.py
@@ -130,4 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
 'django.contrib.auth.backends.ModelBackend',
 'account.authentication.EmailAuthBackend',
+'social_core.backends.google.GoogleOAuth2',
 ]
